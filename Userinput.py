b457cb9ad7654e15ab10d209e0602c93bbbeaed2
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-14 10:18:17 +0530

    Update Userinput.py

--- Userinput.py
+++ Userinput.py
@@ -3,3 +3,8 @@ print(name)
 #type casting : is process to converting one data type to other datatype
 age = int(input("Enter your age: "))
 print(type(age))
+#output:
+Enter your Name: Anurag
+Anurag
+Enter your age: 18
+<class 'int'>
